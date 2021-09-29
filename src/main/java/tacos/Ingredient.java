c8799334106ded690cb136a1b1b8649b18eadd3f
alandill <alandill@rocketmail.com>
2021-09-29 17:17:00 -0300

    added constructor, getters and setters. Lombok not working

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -8,6 +8,22 @@ public class Ingredient {
     private final String name = "";
     private final Type type = null;
 
+    //For some reason lombok is not working, if/when it works we can erase the getters and constructor
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public Ingredient(String id, String name, Type type) {
+    }
+
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
     }
